aa71b2706388682280a5a0ebd06d36ef8ea63e37
Albert <albertvicentegarr@gmail.com>
2025-10-05 13:12:12 +0200

    Complete de calculator, def, square, return, pow()

--- week-0/calculator.py
+++ week-0/calculator.py
@@ -1,6 +1,6 @@
 #the int() is for put a type int (4) and float() is for put a type float (4.2)
-x = float(input("What's x? "))
-y = float(input("What's y? "))
+#x = float(input("What's x? "))
+#y = float(input("What's y? "))
 
 #plus two numbers
 #z = x + y
@@ -8,6 +8,18 @@ y = float(input("What's y? "))
 #the round is for round a digit, for example 1.5 + 4.7 = 6,2 round(6)
 #z = round(x + y )
 
-z = x / y
+#z = x / y
 
-print(f"{z:.2f}")
+#print(f"{z:.2f}")
+
+
+
+
+def main():
+    x = int(input("What's x? "))
+    #y = int(input("What's x? "))
+    print("x squared is", square(x))
+    
+def square(n):
+    return pow(n, 2)
+main()
