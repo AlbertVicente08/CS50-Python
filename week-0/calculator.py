d152144b2a55622893f8498688d8bf0fbf02ea3d
Albert <albertvicentegarr@gmail.com>
2025-10-05 12:42:17 +0200

    Calculator in progress, we see the int() the float(), two numbers plus, two numbers division, and we see the round() and a round whit f string

---
+++ week-0/calculator.py
@@ -0,0 +1,13 @@
+#the int() is for put a type int (4) and float() is for put a type float (4.2)
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+#plus two numbers
+#z = x + y
+
+#the round is for round a digit, for example 1.5 + 4.7 = 6,2 round(6)
+#z = round(x + y )
+
+z = x / y
+
+print(f"{z:.2f}")
