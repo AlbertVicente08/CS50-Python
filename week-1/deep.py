15eb3f07877f02807827f0edc11fbc81a4e0f665
Albert <albertvicentegarr@gmail.com>
2025-10-06 22:28:54 +0200

    week 1 semi-complete

---
+++ week-1/deep.py
@@ -0,0 +1,6 @@
+life = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower()
+
+if life == "42" or life == "forty-two" or life == "forty two":
+    print("yes")
+else:
+    print("No")
